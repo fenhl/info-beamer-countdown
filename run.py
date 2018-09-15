c9f9c0bbcbadd85a2f582f151f7f08f2a531e20b
Fenhl <fenhl@fenhl.net>
2018-09-15 14:33:09 +0000

    Fix syntax error

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     shutil.copy2(gitdir.host.github.GitHub().repo('fenhl/info-beamer-text').branch_path() / 'text.lua', NODE_DIR / 'text.lua')
     # run node
     target_date = timespec.parse(sys.argv[1:])
-    popen = subprocess.Popen(['info-beamer', str(NODE_DIR))
+    popen = subprocess.Popen(['info-beamer', str(NODE_DIR)])
     time.sleep(1)
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.sendto('countdown/delta/set:{}'.format((target_date - datetime.datetime.now(datetime.timezone.utc)).total_seconds()).encode('utf-8'), ('127.0.0.1', 4444))
