e0ac8a647e94212f923103bd1bbe77cb98f80689
Fenhl <fenhl@fenhl.net>
2018-09-15 14:30:09 +0000

    Fix node directory

--- run.py
+++ run.py
@@ -12,13 +12,15 @@ import subprocess
 import time
 import timespec
 
+NODE_DIR = pathlib.Path(__file__).parent
+
 if __name__ == '__main__':
     # copy dependencies into the node directory because some info-beamer versions don't support symlinks
-    shutil.copy2('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', pathlib.Path(__file__).parent / 'dejavu_sans.ttf')
-    shutil.copy2('/opt/git/github.com/fenhl/info-beamer-text/master/text.lua', pathlib.Path(__file__).parent / 'text.lua')
+    shutil.copy2('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', NODE_DIR / 'dejavu_sans.ttf')
+    shutil.copy2('/opt/git/github.com/fenhl/info-beamer-text/master/text.lua', NODE_DIR / 'text.lua')
     # run node
     target_date = timespec.parse(sys.argv[1:])
-    popen = subprocess.Popen(['info-beamer', '/home/fenhl/info-beamer-countdown'])
+    popen = subprocess.Popen(['info-beamer', str(NODE_DIR))
     time.sleep(1)
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.sendto('countdown/delta/set:{}'.format((target_date - datetime.datetime.now(datetime.timezone.utc)).total_seconds()).encode('utf-8'), ('127.0.0.1', 4444))
